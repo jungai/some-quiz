1e636b96525060ff5df4accbb0786162f60f0536
jungai <juworaphol125@gmail.com>
2022-03-09 18:49:23 +0700

    add Card component

---
+++ front_end/src/components/Card/Card.tsx
@@ -0,0 +1,32 @@
+import type { FC } from "react";
+import {
+	StyledCardContainer,
+	StyledCardImg,
+	StyledCardImgWrapper,
+	StyledCardTextTitle,
+	StyledCardTextWrapper,
+	StyledCardTextLocation,
+} from "./CardStyles";
+
+export interface CardProps {
+	imgSrc?: string;
+	title?: string;
+	location?: string;
+	onClick?: () => void;
+}
+
+export const Card: FC<CardProps> = ({ imgSrc, title, location, onClick }) => {
+	return (
+		<StyledCardContainer onClick={onClick}>
+			<StyledCardImgWrapper>
+				<StyledCardImg src={imgSrc} />
+			</StyledCardImgWrapper>
+			<StyledCardTextWrapper>
+				<StyledCardTextTitle>{title}</StyledCardTextTitle>
+				<StyledCardTextLocation>{location}</StyledCardTextLocation>
+			</StyledCardTextWrapper>
+		</StyledCardContainer>
+	);
+};
+
+export default Card;
---
+++ front_end/src/components/Card/CardStyles.tsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+import { size } from "../../utils/devices";
+
+export const StyledCardContainer = styled.div`
+	border-radius: 1rem;
+	overflow: hidden;
+	border: 1px solid #ccc;
+	height: 280px;
+	width: 100%;
+`;
+
+export const StyledCardImgWrapper = styled.div`
+	height: 60%;
+`;
+
+export const StyledCardImg = styled.img`
+	width: 100%;
+	height: 100%;
+	object-fit: cover;
+`;
+
+export const StyledCardTextWrapper = styled.div`
+	margin: 0.5rem;
+	width: 100%;
+`;
+
+// TODO: variable thing ...
+export const StyledCardTextTitle = styled.h3`
+	font-size: 1rem;
+	font-weight: 500;
+
+	@media (min-width: ${size.md}) {
+		font-size: 1.4rem;
+	}
+`;
+
+export const StyledCardTextLocation = styled.p`
+	font-size: 0.8rem;
+
+	@media (min-width: ${size.md}) {
+		font-size: 1rem;
+	}
+`;
