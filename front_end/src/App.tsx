cec62cb9cb950fcfbab68a99d2bcaa8f9b831834
jungai <juworaphol125@gmail.com>
2022-03-09 18:53:02 +0700

    update App with react router

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,5 +1,24 @@
-function App() {
-  return <h1>Hello</h1>;
-}
+import type { FC } from "react";
+import { lazy, Suspense } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Index from "./views";
+
+const ReservationPage = lazy(() => import("./views/Reservation"));
+
+export const App: FC = () => {
+	return (
+		<Routes>
+			<Route path="/" element={<Index />} />
+			<Route
+				path="reservation/:id"
+				element={
+					<Suspense fallback="loading...">
+						<ReservationPage />
+					</Suspense>
+				}
+			/>
+		</Routes>
+	);
+};
 
 export default App;
