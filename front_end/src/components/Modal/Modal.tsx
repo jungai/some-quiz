b3b4eccf29e25142a8ebb918ff957586c6092c04
jungai <juworaphol125@gmail.com>
2022-03-10 20:54:43 +0700

    add Modal Component

---
+++ front_end/src/components/Modal/Modal.tsx
@@ -0,0 +1,36 @@
+import { FC, useEffect } from "react";
+import Card from "../Card";
+import {
+	StyledModal,
+	StyledModalWrapper,
+	StyledModalCloseBtn,
+} from "./ModalStyles";
+import { AiOutlineClose } from "react-icons/ai";
+
+export interface ModalProps {
+	open?: boolean;
+	onClose?: () => void;
+}
+
+export const Modal: FC<ModalProps> = ({ children, open, onClose }) => {
+	useEffect(() => {
+		document.body.style.overflow = "hidden";
+
+		return () => {
+			document.body.style.overflow = "visible";
+		};
+	}, []);
+
+	return open ? (
+		<StyledModal>
+			<StyledModalWrapper>
+				<Card clean>{children}</Card>
+				<StyledModalCloseBtn onClick={onClose}>
+					<AiOutlineClose />
+				</StyledModalCloseBtn>
+			</StyledModalWrapper>
+		</StyledModal>
+	) : null;
+};
+
+export default Modal;
---
+++ front_end/src/components/Modal/ModalStyles.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const StyledModal = styled.div`
+	z-index: 50;
+	position: fixed;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	padding: 1rem;
+	top: 0;
+	left: 0;
+	height: 100vh;
+	width: 100vw;
+	background: rgba(0, 0, 0, 0.5);
+`;
+
+export const StyledModalWrapper = styled.div`
+	z-index: 60;
+	position: relative;
+`;
+
+export const StyledModalCloseBtn = styled.div`
+	position: absolute;
+	top: -10px;
+	right: -5px;
+	border-radius: 50%;
+	border: 1px solid #ccc;
+	background-color: white;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 25px;
+	width: 25px;
+`;
